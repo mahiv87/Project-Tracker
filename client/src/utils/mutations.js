1aa4ba30b2549d2f94fcbab9a6bb5a8d767f5f4a
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-24 12:10:20 -0600

    queries and mutations added in client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,56 +1,57 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				email
+			}
+		}
+	}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation addUser($email: String!, $password: String!) {
+		addUser(email: $email, password: $password) {
+			token
+			user {
+				_id
+				email
+			}
+		}
+	}
 `;
 
-export const ADD_THOUGHT = gql`
-  mutation addThought($thoughtText: String!) {
-    addThought(thoughtText: $thoughtText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-      }
-    }
-  }
+export const SAVE_PROJECT = gql`
+	mutation saveProject($project: inputProject!) {
+		saveProject(project: $project) {
+			email
+			projectCount
+			savedProjects {
+				projectId
+				projectName
+				projectType
+				due
+				rate
+			}
+		}
+	}
 `;
 
-export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
-    }
-  }
+export const REMOVE_PROJECT = gql`
+	mutation removeProject($projectId: Int!) {
+		removeProject(projectId: $projectId) {
+			email
+			projectCount
+			savedProjects {
+				projectId
+				projectName
+				projectType
+				due
+				rate
+			}
+		}
+	}
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,15 +1,16 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql`
+export const GET_ME = gql`
 	query me {
 		me {
-			_id
 			email
-			thoughts {
-				_id
-				thoughtText
-				thoughtAuthor
-				createdAt
+			projectCount
+			savedProjects {
+				projectId
+				projectName
+				projectType
+				due
+				rate
 			}
 		}
 	}
