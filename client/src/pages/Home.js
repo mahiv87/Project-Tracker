369a452eb8b89fa27b48e42121e9d31b36f533bb
Marcus Herrera <maherreraiv1987@gmail.com>
2022-08-08 12:30:15 -0600

    changed home page instructions

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 			<section className="container mx-auto flex flex-col justify-center items-center text-center w-full h-[calc(100vh-5rem)]">
 				<h1 className="text-6xl font-thasadith animate-fadeIn">Welcome to Project Tracker</h1>
 				<p className="text-center my-2 text-neutral-500 italic opacity-0 animate-fadeInLong">
-					login to continue
+					login or signup to continue
 				</p>
 			</section>
 		</>
