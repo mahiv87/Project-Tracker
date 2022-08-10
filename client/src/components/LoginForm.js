be9d09c822fa545b68be012b2e4513e32ba4cdfc
Marcus Herrera <maherreraiv1987@gmail.com>
2022-08-09 20:56:11 -0600

    debugged login/signup message

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,8 +6,6 @@ import Auth from '../utils/auth';
 
 const LoginForm = () => {
 	const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-	// const [validated] = useState(false);
-	// const [showAlert, setShowAlert] = useState(false);
 	const [login, { error, data }] = useMutation(LOGIN_USER);
 
 	const handleInputChange = (event) => {
